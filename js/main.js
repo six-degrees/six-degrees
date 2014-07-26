bf3fc66417928ac6c2974ad7dfbd7141cd966e51
Gunther Cox <gunthercx@gmail.com>
2014-07-26 16:15:21 -0400

    Added test with recursive json calls

--- js/main.js
+++ js/main.js
@@ -13,3 +13,34 @@ $startingUser.add($endingUser).on("keyup", function () {
         $btnConnectTheDots.attr("tabindex", "-1");
     }
 });
+
+
+var maxConnections = 6 * 6;
+
+function getFollowers(username) {
+
+    maxConnections -= 1;
+
+    var user_url = "data/followers/json/" + username + ".json";
+
+    var request = $.ajax({
+        url: user_url,
+        contentType: "application/json",
+        type: "GET"
+    });
+
+    request.then(function(data) {
+        if (maxConnections >= 0) {
+            console.log(data.following);
+            for (var i = 0; i < data.following.length; i++) {
+                console.log("follower:", data.following[i]);
+                getFollowers(data.following[i]);
+            }
+        }
+    });
+
+}
+
+// Testing start user
+var username = "gunthercox";
+getFollowers(username)
