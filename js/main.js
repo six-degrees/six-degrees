6e21e67b2eb07b88d3792c17a4f87f84661cd4b4
Kevin Brown <kevin@kevinbrown.in>
2014-07-26 18:57:55 -0400

    Slightly better performance for getting the pathes
    This also limits the active requests and appear to have fixed the
    fact that Chrome blanks the page if there are too many.  Still
    doesn't perform well outside of 3 degrees and missed the user once
    or twice.

--- js/main.js
+++ js/main.js
@@ -14,40 +14,130 @@ $startingUser.add($endingUser).on("keyup", function () {
     }
 });
 
+$btnConnectTheDots.on("click", function (evt) {
+    evt.preventDefault();
 
-var maxConnections = Math.pow(6, 6);
-var userQue = [];
+    foundChain = [];
+    activeDegree = 1;
+    activeConnections = 0;
+    userQueue = {};
+
+    scanDegree([$startingUser.val()], $endingUser.val());
+});
+
+var userCache = {};
+var userQueue = {};
+
+var activeConnections = 0;
+var maxConnections = 10;
+
+var activeDegree = 1;
+
+var foundChain = [];
+
+function scanDegree(chain, ending) {
+    if (foundChain.length > 0) {
+        return;
+    }
+
+    var currentUser = chain[chain.length - 1];
+
+    var userData = checkUser(chain.length, currentUser);
+
+    userData.then(function (data) {
+        for (var u = 0; u < data.following.length; u++) {
+            var follower = data.following[u];
+
+            var clonedChain = chain.slice();
+
+            clonedChain.push(follower);
+
+            if (follower == ending) {
+                displayChain(clonedChain);
+                return;
+            }
+
+            scanDegree(clonedChain, ending);
+        }
+    });
+}
+
+function displayChain(chain) {
+    foundChain = chain;
+    console.log(chain);
+}
+
+function checkUser(degree, username) {
+    var $p = $.Deferred();
+
+    if (!(degree in userQueue)) {
+        userQueue[degree] = [];
+    }
+
+    userQueue[degree].push([username, $p]);
+
+    checkQueue();
+
+    return $p;
+}
+
+function checkQueue () {
+    if (foundChain.length > 0) {
+        return;
+    }
+
+    var queue = userQueue[activeDegree];
+
+    if (queue.length == 0) {
+        if (activeConnections == 0) {
+            activeDegree++;
+            return;
+        } else {
+            window.setTimeout(checkQueue, 100);
+            return;
+        }
+    }
+
+    console.log(activeDegree, activeConnections, maxConnections);
+
+    if (activeConnections < maxConnections) {
+        var userData = queue.shift();
+
+        if (userData == null) {
+            return;
+        }
+
+        activeConnections += 1;
+
+        var request = getFollowers(userData[0]);
+        var $p = userData[1];
+
+        request.then(function (data) {
+            $p.resolve(data);
+        })
+
+        request.always(function () {
+            activeConnections -= 1;
+        });
+    }
+}
 
 function getFollowers(username) {
+    if (username in userCache) {
+        return userCache[username];
+    }
 
-    maxConnections -= 1;
-
-    var user_url = "data/followers/json/" + username + ".json";
+    var userUrl = "data/followers/json/" + username + ".json";
 
     var request = $.ajax({
-        url: user_url,
+        url: userUrl,
         contentType: "application/json",
         type: "GET"
     });
 
-    request.then(function(data) {
-
-        // If there is no data don't continue
-        if (data.following.length < 1) {
-            return
-        }
-
-        if (maxConnections >= 0) {
-            console.log(data.following);
-            for (var i = 0; i < data.following.length; i++) {
-                console.log("follower:", data.following[i]);
-                getFollowers(data.following[i]);
-            }
-        }
+    request.always(function(data) {
+        userCache[username] = request;
     });
 
+    return request;
 }
-
-// Testing start user
-var username = "gunthercox";
-getFollowers(username)
