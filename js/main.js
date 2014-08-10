297dcd7c1bb37cdc4baac74057fcadfc707154f6
Kevin Brown <kevin@kevinbrown.in>
2014-08-10 17:30:34 -0400

    Performance fixes
    This is the first shot at improving the memory usage of searches
    and helping the garbage collector do its job.  Additional
    profiling should be done in the future, after the way searching
    is done has been improved, to identify further areas to improve
    the search performance and memory usage.
    
    ====
    
    Instead of caching the jQuery requests used to retrieve the
    followers after they have gone through, a boolean `true` or
    `false` is now being cached.  The `getFollowers` method will
    still return this cached boolean if the request for followers
    has already been made, or the jQuery request promise if the
    request has not been made before.
    
    This also has the additional side-effect of limiting the number
    of duplicated branches that are followed.  Now, instead of
    following the branch along cached pathes, the branch is not
    followed again.  Because of how the search is done, this should
    not have an impact on calculating the chain, as branches other
    than the first that are found will either have the same degree
    as previous ones, or a larger degree.  This also greatly lowers
    the size of the queue when searching along higher number of
    degrees.
    
    Because of the social nature of GitHub, and other social
    networking-based websites, it is very likely that users will
    share similar followers in higher degrees.  This reduces the
    number of branches that must be checked, and as a result has a
    significant speed increase when searching along higher degrees.
    
    With this change, the user data can no longer be cached across
    searches, which had previously resulted in large increases in
    memory usage without the ability to clear the cache without
    reloading the page.
    
    ====
    
    Users are now shifted off of the queue using `splice(0, 1)` instead
    of `shift`.  In large arrays, such as the one that we encounter
    in higher degrees, this has a considerable performance gain when
    processing it.
    
    ====
    
    This also allows users to stop the search by hitting the search
    button a second time while the search is in progress.  This will
    stop the queue as soon as possible and clear up any data variables
    used during the search.  This required adding a `searchChain`
    boolean which will be `true` when a search is in progress and
    `false` when a search should not be done.
    
    ====
    
    This also adds some quick documentation to a few of the main
    functions in a docstring format.  This should make it more clear
    for those interested in contributing to the project to understand
    what the methods do.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ var $endingUser = $(".js-ending-user-login");
 var $connectTheDots = $(".js-connect-dots");
 var $btnConnectTheDots = $(".js-btn-connect-dots");
 
+/*
+ Register a check on the `keyup` event of the starting and ending user input
+ boxes.
+
+ This will show the button to start the search if both the starting user and
+ ending user are not blank.  It will also push the button into the tab order, so
+ users can immediately tab into the button after entering the ending user.
+
+ If either the starting user or the ending user is not filled in, the button
+ will be hidden and it will be removed from the tab order.
+ */
 $startingUser.add($endingUser).on("keyup", function () {
     if ($.trim($startingUser.val()) && $.trim($endingUser.val())) {
         $connectTheDots.addClass("connectable");
@@ -17,10 +28,20 @@ $startingUser.add($endingUser).on("keyup", function () {
 $btnConnectTheDots.on("click", function (evt) {
     evt.preventDefault();
 
+    // Only stop the queue if the search is still running.
+    if ((activeConnections > 0 || activeDegree > 1) && searchChain) {
+        stopSearch();
+
+        console.log("Stopped the search");
+
+        return;
+    }
+
+    searchChain = true;
+
     foundChain = [];
-    activeDegree = 1;
-    activeConnections = 0;
-    userQueue = {};
+
+    clearData();
 
     scanDegree([$startingUser.val()], $endingUser.val());
 });
@@ -34,6 +55,7 @@ var maxConnections = 10;
 var activeDegree = 1;
 
 var foundChain = [];
+var searchChain = false;
 
 function scanDegree(chain, ending) {
     if (foundChain.length > 0) {
@@ -62,11 +84,27 @@ function scanDegree(chain, ending) {
     });
 }
 
+/*
+ Display the chain of users that can be used to connect the starting user to the
+ ending user.  The first element passed in the array should be the starting
+ user, and the last element should be the ending user.  Any other elements
+ should appear in the array in the order that can be used to create the chain
+ again.
+ */
 function displayChain(chain) {
+    searchChain = false;
     foundChain = chain;
     console.log(chain);
 }
 
+/*
+ Push a user into the checking queue under a specific degree.  This will also
+ fire off the queue check to ensure the queue is always running when users
+ should be searched.
+
+ A promise will be returned that will resolve (with the follower data) if the
+ user was found.  If the user was not found, the promise will never resolve.
+ */
 function checkUser(degree, username) {
     var $p = $.Deferred();
 
@@ -82,7 +120,7 @@ function checkUser(degree, username) {
 }
 
 function checkQueue () {
-    if (foundChain.length > 0) {
+    if (!searchChain) {
         return;
     }
 
@@ -101,16 +139,31 @@ function checkQueue () {
     console.log(activeDegree, activeConnections, maxConnections);
 
     if (activeConnections < maxConnections) {
-        var userData = queue.shift();
+        var userData = queue.splice(0, 1);
 
-        if (userData == null) {
+        if (userData.length === 0) {
             return;
         }
+
+        userData = userData[0];
 
         activeConnections += 1;
 
         var request = getFollowers(userData[0]);
         var $p = userData[1];
+
+        // If the user has already been searched, ignore the promise
+        // This prevents us from searching the same branch multiple times
+        if (request === true) {
+            activeConnections -= 1;
+            return;
+        }
+
+        // If the user could not be found before, assume it can't be found again
+        if (request === false) {
+            activeConnections -= 1;
+            return;
+        }
 
         request.then(function (data) {
             $p.resolve(data);
@@ -122,6 +175,17 @@ function checkQueue () {
     }
 }
 
+/*
+ Try to make a request to get the followers for a user.
+
+ Note:
+ - This will return `true` if the request has already been made and the list of
+   followers could be retrieved.
+ - This will return `false` if the request has already been made and the list of
+   followers could not be retrieved.
+ - This will return a jQuery promise object containing the outgoing request if
+   the request has not already been made.
+ */
 function getFollowers(username) {
     if (username in userCache) {
         return userCache[username];
@@ -135,9 +199,32 @@ function getFollowers(username) {
         type: "GET"
     });
 
-    request.always(function(data) {
-        userCache[username] = request;
+    request.then(function () {
+        userCache[username] = true;
     });
 
+    request.fail(function () {
+        userCache[username] = true;
+    })
+
     return request;
+}
+
+/*
+ Reset the variables used during the search so that another search can be done.
+ */
+function clearData() {
+    activeDegree = 1;
+    activeConnections = 0;
+    userQueue = {};
+    userCache = {};
+}
+
+/*
+ Stop the current search and clear out any data that has been stored.
+ */
+function stopSearch() {
+    searchChain = false;
+
+    clearData();
 }
